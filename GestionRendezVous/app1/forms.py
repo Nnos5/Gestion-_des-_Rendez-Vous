7d4d58f392c0adf832abe90e2ad200d82c3b7b6f
Noubissie237 <wilfried.noubissie@facsciences-uy1.cm>
2023-12-29 00:32:17 +0100

    Fixed some bugs

---
+++ GestionRendezVous/app1/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+from .models import *
+
+class FormDonnee(forms.ModelForm):
+    
+    class Meta:
+        model = Donnee
+        fields = ['nom','prenom','email','sexe','age']
+
+    widgets ={
+
+           "nom":forms.TextInput(attrs={'class':'form-control','placeholder':'NOM',}),
+            "prenom":forms.TextInput(attrs={'class':'form-control','placeholder':'PRENOM'}),
+            "email":forms.EmailInput(attrs={'class':'form-control' ,'placeholder':'EMAIL'}),
+            "sexe":forms.Select(attrs={'class':'form-control','placeholder':'SEXE'}),
+            "age":forms.NumberInput(attrs={'class':'form-control' ,'placeholder':'AGE'}),
+    }   
+
+
+class FormRdv(forms.ModelForm):
+    class Meta:
+        model = RDV
+        fields = ['nom','prenom','email','sexe','age','Date','Heure']
+        
+
+    widgets = {
+
+            "nom":forms.TextInput(attrs={'class':'form-control','placeholder':'NOM',}),
+            "prenom":forms.TextInput(attrs={'class':'form-control','placeholder':'PRENOM'}),
+            "email":forms.EmailInput(attrs={'class':'form-control' ,'placeholder':'AGE'}),
+            "sexe":forms.Select(attrs={'class':'form-control','placeholder':'SEXE'}),
+            "age":forms.NumberInput(attrs={'class':'form-control' ,'placeholder':'AGE'}),
+            "Date":forms.DateInput(attrs={'class':'form-control' ,'placeholder':'DATE'}),
+            "Heure":forms.TimeInput(attrs={'class':'form-control' ,'placeholder':'HEURE'}),
+    }   
